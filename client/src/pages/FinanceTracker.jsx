ab05d330c08a6ef8342c722f2b8c160c52dff240
fahim.hadi <fahimhadimaula@gmail.com>
2024-01-09 17:23:14 +0700

    succeed to apply Create - Update

--- client/src/pages/FinanceTracker.jsx
+++ client/src/pages/FinanceTracker.jsx
@@ -11,7 +11,7 @@ const FormFinanceTracker = () => {
   const [amount, setAmount] = useState();
   const [date, setDate] = useState();
 
-	const [txn, setTxn] = useState([]);
+  const [txn, setTxn] = useState([]);
 
   /**
    * @param {React.FormEvent} e
@@ -26,107 +26,134 @@ const FormFinanceTracker = () => {
         amount,
         date,
       }),
-			headers: {
-				"Content-Type": "application/json",
-			}
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-		setWallet("");
-		setExpense("");
-		setAmount("");
-		setDate("");
+    setWallet("");
+    setExpense("");
+    setAmount("");
+    setDate("");
   }
 
-	useEffect(() => {
-		async function fetchTxn() {
-			const response = await fetch("http://localhost:3001/txn");
-			const newTxn = await response.json();
-			setTxn(newTxn);
-		}
-		fetchTxn();
-	}, []);
+  useEffect(() => {
+    async function fetchTxn() {
+      const response = await fetch("http://localhost:3001/txn");
+      const newTxn = await response.json();
+      setTxn(newTxn);
+    }
+    fetchTxn();
+  }, []);
+
+	const formatDate = (originalDate) => {
+		const date = new Date(originalDate);
+		const year = date.getFullYear();
+		const month = String(date.getMonth() + 1).padStart(2, '0');
+		const day = String(date.getDate()).padStart(2, '0');
+
+		return `${year}-${month}-${day}`;
+	};
 
   return (
-		<>
-			<form onSubmit={handleCreateLog}>
-				<label htmlFor="wallet" className={labelClass}>
-					wallet
-				</label>
-				<input
-					type="text"
-					id="wallet"
-					value={wallet}
-					/**
-					 * @param {React.ChangeEvent<HTMLInputElement>}
-					 */
-					onChange={(e) => {
-						setWallet(e.target.value);
-					}}
-					placeholder="Type your wallet..."
-					className={inputClass}
-				/>
+    <>
+      <form onSubmit={handleCreateLog}>
+        <label htmlFor="wallet" className={labelClass}>
+          wallet
+        </label>
+        <input
+          type="text"
+          id="wallet"
+          value={wallet}
+          /**
+           * @param {React.ChangeEvent<HTMLInputElement>}
+           */
+          onChange={(e) => {
+            setWallet(e.target.value);
+          }}
+          placeholder="Type your wallet..."
+          className={inputClass}
+        />
 
-				<label htmlFor="expense" className={labelClass}>
-					expense
-				</label>
-				<input
-					type="text"
-					id="expense"
-					value={expense}
-					/**
-					 * @param {React.ChangeEvent<HTMLInputElement>}
-					 */
-					onChange={(e) => {
-						setExpense(e.target.value);
-					}}
-					placeholder="Type your expense..."
-					className={inputClass}
-				/>
+        <label htmlFor="expense" className={labelClass}>
+          expense
+        </label>
+        <input
+          type="text"
+          id="expense"
+          value={expense}
+          /**
+           * @param {React.ChangeEvent<HTMLInputElement>}
+           */
+          onChange={(e) => {
+            setExpense(e.target.value);
+          }}
+          placeholder="Type your expense..."
+          className={inputClass}
+        />
 
-				<label htmlFor="amount" className={labelClass}>
-					amount
-				</label>
-				<input
-					type="text"
-					id="amount"
-					value={amount}
-					/**
-					 * @param {React.ChangeEvent<HTMLInputElement>}
-					 */
-					onChange={(e) => {
-						setAmount(e.target.value);
-					}}
-					placeholder="How much your expense???"
-					className={inputClass}
-				/>
+        <label htmlFor="amount" className={labelClass}>
+          amount
+        </label>
+        <input
+          type="text"
+          id="amount"
+          value={amount}
+          /**
+           * @param {React.ChangeEvent<HTMLInputElement>}
+           */
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+          placeholder="How much your expense???"
+          className={inputClass}
+        />
 
-				<label htmlFor="date" className={labelClass}>
-					date
-				</label>
-				<input
-					type="text"
-					id="date"
-					value={date}
-					/**
-					 * @param {React.ChangeEvent<HTMLInputElement>}
-					 */
-					onChange={(e) => {
-						setDate(e.target.value);
-					}}
-					placeholder="When?"
-					className={inputClass}
-				/>
+        <label htmlFor="date" className={labelClass}>
+          date
+        </label>
+        <input
+          type="text"
+          id="date"
+          value={date}
+          /**
+           * @param {React.ChangeEvent<HTMLInputElement>}
+           */
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+          placeholder="When?"
+          className={inputClass}
+        />
 
-				<button className="mt-8 w-[150px] bg-sky-950 h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-zinc-400 tracking-wider">
-					Submit
-				</button>
-			</form>
+        <button className="mt-8 w-[150px] bg-sky-950 h-[50px] my-3 flex items-center justify-center rounded-xl cursor-pointer relative overflow-hidden transition-all duration-500 ease-in-out shadow-md hover:scale-105 hover:shadow-lg before:absolute before:top-0 before:-left-full before:w-full before:h-full before:bg-gradient-to-r before:from-[#009b49] before:to-[rgb(105,184,141)] before:transition-all before:duration-500 before:ease-in-out before:z-[-1] before:rounded-xl hover:before:left-0 text-zinc-400 tracking-wider hover:text-slate-950">
+          Submit
+        </button>
+      </form>
 
-			<ul>
-				{txn.map((_txn) => (
-					<li key={_txn._id}>{_txn.wallet}</li>
-				))}
-			</ul>
-		</>
+      <h2 className="mt-10 font-semibold tracking-widest mb-0">
+        Transaction History
+      </h2>
+      <table className="border-collapse border border-slate-500">
+        <thead>
+          <tr>
+            <th className="border border-slate-600 bg-slate-950">Date</th>
+            <th className="border border-slate-600 bg-slate-950">Wallet</th>
+            <th className="border border-slate-600 bg-slate-950">Expense</th>
+            <th className="border border-slate-600 bg-slate-950">Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {txn.map((item) => (
+            <tr key={item._id} className="hover:bg-slate-800">
+              <td className="border border-slate-700 px-2">{formatDate(item.date)}</td>
+              <td className="border border-slate-700 px-2">{item.wallet}</td>
+              <td className="border border-slate-700 px-2">{item.expense}</td>
+              <td className="border border-slate-700 px-2">{item.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
